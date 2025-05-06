502827fb546bdca4d632000954de0ae893254ac8
LEE YOU BIN <128459613+leeeyubin@users.noreply.github.com>
2025-05-07 02:30:07 +0900

    경로 찾기

---
+++ Silver/Boj11403.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+        int[][] graph = new int[N][N];
+
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j ++) {
+                graph[i][j] = scanner.nextInt();
+            }
+        }
+
+        for (int k = 0; k < N; k++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (graph[i][k] == 1 && graph[k][j] == 1) {
+                        graph[i][j] = 1;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                System.out.print(graph[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
