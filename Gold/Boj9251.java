33c6cc4f8f0e8c1080ae9e52cc39d1b5ff664b0e
LEE YOU BIN <128459613+leeeyubin@users.noreply.github.com>
2025-08-28 06:09:58 +0900

    LCS

---
+++ Gold/Boj9251.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+
+    static char[] str1, str2;
+    static int[][] dp;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        str1 = scanner.nextLine().toCharArray();
+        str2 = scanner.nextLine().toCharArray();
+
+        dp = new int[str1.length][str2.length];
+
+        for (int i = 1; i <= str1.length; i++) {
+            for (int j = 1; j <= str2.length; j++) {
+                if (str1[i - 1] == str2[j - 1]) dp[i][j] = dp[i - 1][j - 1] + 1;
+                else dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+
+        System.out.println(dp[str1.length][str2.length]);
+
+        scanner.close();
+    }
+}
