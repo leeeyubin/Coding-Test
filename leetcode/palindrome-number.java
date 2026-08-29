298565afc69b459799f82fc4407b51ee8c455b97
LEE YOU BIN <128459613+leeeyubin@users.noreply.github.com>
2026-08-29 17:24:17 +1000

    Implement palindrome number checker in Java

---
+++ leetcode/palindrome-number.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) return false;
+
+        String s = String.valueOf(x);
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while(left < right) {
+            if(s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+
+            right --;
+            left ++;
+        }
+
+        return true;
+    }
+}
