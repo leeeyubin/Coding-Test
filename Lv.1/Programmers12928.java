57b70185072aa90c7c08cd4a5cda26318af7ccb9
LEE YOU BIN <128459613+leeeyubin@users.noreply.github.com>
2026-09-08 22:38:56 +1000

    Implement solution to find sum of divisors

---
+++ Lv.1/Programmers12928.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        if(n == 0) return 0;
+        
+        for(int i = 2; i <= n; i++) {
+            if(n % i == 0) answer += i;
+        }
+        
+        answer += 1;
+        
+        return answer;
+    }
+}
